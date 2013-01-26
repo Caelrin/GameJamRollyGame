4fb36121ebbcb81d18b1e959e31a8d61d9845eba
Dan Miller <danmiller126407@gmail.com>
2013-01-25 20:27:59 -0500

    intial Unity project commit
    movement scene,
    player movment,
    collision script

---
+++ GGJ13/Assets/Scripts/Movement.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+
+    public GameObject player;
+    public float movespeed;
+    public Vector3 velocity = new Vector3(0,0,0);
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (Input.GetKey(KeyCode.D))
+        {
+           velocity += new Vector3(movespeed*Time.deltaTime, 0, 0);
+        }
+
+	    if (Input.GetKey(KeyCode.A))
+	    {
+            velocity -= new Vector3(movespeed * Time.deltaTime, 0, 0);
+	    }
+	    if (Input.GetKey(KeyCode.W))
+	    {
+            velocity += new Vector3(0,0, movespeed * Time.deltaTime );
+	    }
+         if (Input.GetKey(KeyCode.S))
+	    {
+            velocity -= new Vector3(0,0, movespeed * Time.deltaTime );
+	    }
+
+
+
+	    player.transform.localPosition += velocity;
+    }
+
+    public Vector3 getVelocity()
+    {
+        return velocity;
+    }
+}
+
---
+++ GGJ13/Assets/Scripts/WallCollision.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+
+public class WallCollision : MonoBehaviour
+{
+
+    public float velocity_set;
+    public bool bounce_back;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision detected");
+
+        if (other.gameObject.name == "Player")
+        {
+            Debug.Log("Collision in velocity zeroed");
+
+            if (bounce_back)
+            {
+                Vector3 temp = other.GetComponent<Movement>().getVelocity();
+                other.GetComponent<Movement>().velocity = -temp/2;
+            }
+            else
+            {
+
+                other.GetComponent<Movement>().velocity = new Vector3(0.0f, 0.0f, 0.0f);
+            }
+        }
+
+
+    }
+}

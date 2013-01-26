e8471b1a4d10efefdeeca0a71beb2738b7c5c96b
Gabbie <Andrea.G.Gibson@gmail.com>
2013-01-26 09:49:02 -0500

    Added Spring and flex to camera

--- GGJ13/Assets/Scripts/CamFollow.cs
+++ GGJ13/Assets/Scripts/CamFollow.cs
@@ -5,11 +5,29 @@ public class CamFollow : MonoBehaviour
 {
 
     public Transform target;
-
+	public GameObject thisCamera;
+	
     void Update()
     {
+		Vector3 diffVector = transform.position - target.position;
+		Vector3 moveVector = (new Vector3(0, 30, -50) - diffVector) / 30;
+		CapCameraBound (diffVector, ref moveVector);
+		transform.position += moveVector;
+		
         transform.LookAt(target);
   
     }
 
+	void CapCameraBound (Vector3 diffVector, ref Vector3 moveVector)
+	{
+		if(diffVector.z > -30) {
+    		moveVector.z = -30 - diffVector.z;
+    	}
+		if(diffVector.z < -100) {
+    		moveVector.z *= -2;
+    	}
+		if(diffVector.x < -50 || diffVector.z < 50) {
+    		moveVector.x *= 2 ;
+    	}
+	}
 }
